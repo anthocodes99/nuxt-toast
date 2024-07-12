594d6e750f2e3de19f48252b69f2d9d0cb6ed0c9
anthocodes99 <anthocodes99@users.noreply.github.com>
2024-07-12 14:24:49 +0800

    build: configure `module.ts` to include `/components` and `/composables`

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver, addComponentsDir, addImportsDir } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
 export interface ModuleOptions {}
@@ -11,9 +11,18 @@ export default defineNuxtModule<ModuleOptions>({
   // Default configuration options of the Nuxt module
   defaults: {},
   setup(_options, _nuxt) {
-    const resolver = createResolver(import.meta.url)
+    const { resolve } = createResolver(import.meta.url)
+
+    // Transpile runtime
+    const runtimeDir = resolve('./runtime')
 
     // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    addPlugin(resolve('./runtime/plugin'))
+    addComponentsDir({
+      path: resolve(runtimeDir, 'components'),
+    })
+
+    // Composables
+    addImportsDir(resolve(runtimeDir, 'composables'))
   },
 })
